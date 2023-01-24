3c5b3e503c1cbcf4aef10760ff3571ae2f443266
Amsavarthan Lv <80940598+amsavarthan@users.noreply.github.com>
2023-01-24 11:29:48 +0530

    Added Custom Font

--- app/src/main/kotlin/com/amsavarthan/tally/presentation/ui/theme/Type.kt
+++ app/src/main/kotlin/com/amsavarthan/tally/presentation/ui/theme/Type.kt
@@ -1,28 +1,20 @@
 package com.amsavarthan.tally.presentation.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import com.amsavarthan.tally.R
+
+val fonts = FontFamily(
+    Font(R.font.light, weight = FontWeight.Light),
+    Font(R.font.regular),
+    Font(R.font.medium, weight = FontWeight.Medium),
+    Font(R.font.semibold, weight = FontWeight.SemiBold),
+    Font(R.font.bold, weight = FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+    defaultFontFamily = fonts,
 )
